a557d4b636fe9fe699320bd6918f0a1d626ba5d0
liujianfu <liujf@baojia.com>
2020-06-05 19:32:08 +0800

    向注册中心注册consuemr

---
+++ ms-eureka-provider/src/main/java/com/ljf/weifuwu/springcloud/eureka/provider/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.ljf.weifuwu.springcloud.eureka.provider.controller;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    @Qualifier("eurekaClient")
+    private EurekaClient eurekaClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/eureka-instance")
+    public String serviceUrl() {
+        //和配置文件配置的application.name=ms-eureka-provider保持一致
+        //获取配置文件的ip和端口号：http://localhost:7900/eureka-instance
+        InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("ms-eureka-provider", false);
+        return instance.getHomePageUrl();
+    }
+    @GetMapping("/instance-info")
+    public ServiceInstance showInfo() {
+        //http://localhost:7900/instance-info
+        //"host":"192.168.1.225","port":7900,"serviceId":"ms-eureka-provider","metadata":{},"secure":false,"uri":"http://192.168.1.225:7900"}
+        ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+        return localServiceInstance;
+    }
+}
