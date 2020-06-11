e77c94e9508e429d315d6a07e36947fc2207e066
liujianfu <liujf@baojia.com>
2020-06-11 21:40:44 +0800

    dashboard 单体指标的显示

---
+++ ms-turbine-consumer/src/main/java/com/ljf/weifuwu/springcloud/turbine/App.java
@@ -0,0 +1,13 @@
+package com.ljf.weifuwu.springcloud.turbine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
